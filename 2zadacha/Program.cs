d40bc6545f1c3a9cffc2ec7a4c4ab40db4260969
Vika <vica_che@mail.ru>
2022-09-22 18:27:05 +0300

    2zadacha

---
+++ 2zadacha/Program.cs
@@ -0,0 +1,11 @@
+﻿int n, m, s=0;
+Console.Write("Введи значение от: ");
+n = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи значение до: ");
+m = Convert.ToInt16(Console.ReadLine());
+for (int i = n; i <= m; i++)
+{
+if (i % 1 == 0)
+s+=i;
+}
+Console.WriteLine(s);
